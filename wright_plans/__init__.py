b1a173b6c52ec7666c937168be163fa7c12fdad5
Kyle Sunden <git@ksunden.space>
2021-09-07 21:47:11 -0500

    Update version

--- wright_plans/__init__.py
+++ wright_plans/__init__.py
@@ -1,6 +1,6 @@
 """Bluesky plans and utilities to provide functionality that the Wright Group needs."""
 
-__version__ = "2021.5.0"
+__version__ = "2021.9.0"
 
 from ._plans import *
 from ._constants import *
