5d77c4f232550f34e5cb529d953c863d9310cb72
pcruzparri <43578034+pcruzparri@users.noreply.github.com>
2022-07-26 21:29:10 -0500

    2022.7.0 (#21)
    * release 2022.7.0 prep changes
    
    * update version num in init
    
    Co-authored-by: Kyle Sunden <git@ksunden.space>

--- wright_plans/__init__.py
+++ wright_plans/__init__.py
@@ -1,6 +1,6 @@
 """Bluesky plans and utilities to provide functionality that the Wright Group needs."""
 
-__version__ = "2022.6.0"
+__version__ = "2022.7.0"
 
 from ._plans import *
 from ._constants import *
